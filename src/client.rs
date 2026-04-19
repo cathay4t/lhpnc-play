90d250c811be7fe2e8f2d511a24c819f3e522d1c
Gris Ge <cnfourt@gmail.com>
2026-04-19 08:52:52 +0800

    CI: Fix CI by install protobuf compiler
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -11,17 +11,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
     let outbound = async_stream::stream! {
-        let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
+        let mut interval =
+            tokio::time::interval(std::time::Duration::from_secs(1));
         let mut count = 0;
 
-        while let time = interval.tick().await {
+        loop {
+            interval.tick().await;
             let request = HelloRequest {
                 name: format!("client_{count}"),
             };
             count +=1;
             yield request;
         }
-
     };
 
     let response = client.say_hello(tonic::Request::new(outbound)).await?;
